fe50e03d98379cf9cc5c9d377dff357fcbe09424
ANERI\Aneri <aneri.parikh@infostretch.com>
2018-07-07 19:43:38 +0530

    Added object utils and constants file

---
+++ src/main/java/com/cognitivescale/util/Constants.java
@@ -0,0 +1,11 @@
+package com.cognitivescale.util;
+
+public class Constants {
+	public static final String ACCOUNT_NOT_REGISTERED = "Entered account number is not registered.";
+	public static final String STATUS_SUCCESS = "Success";
+	public static final String STATUS_ERROR = "Failure";
+
+	private Constants() {
+		throw new IllegalAccessError("Utility class");
+	}
+}
---
+++ src/main/java/com/cognitivescale/util/ResponseUtils.java
@@ -0,0 +1,40 @@
+package com.cognitivescale.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class ResponseUtils {
+
+	private String status;
+	private Object data;
+	private String message;
+
+	public ResponseUtils(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
