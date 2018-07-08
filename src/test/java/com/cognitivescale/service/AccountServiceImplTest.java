1ff394c20589e6fb249cf4fdc06894d97fa0129b
ANERI\Aneri <aneri.parikh@infostretch.com>
2018-07-08 13:39:07 +0530

    Writing testcases for account service

--- src/test/java/com/cognitivescale/service/AccountServiceImplTest.java
+++ src/test/java/com/cognitivescale/service/AccountServiceImplTest.java
@@ -1,10 +1,57 @@
 package com.cognitivescale.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cognitivescale.dao.AccountDao;
+import com.cognitivescale.entity.Account;
+import com.cognitivescale.util.ResponseUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AccountServiceImplTest {
+
+	@TestConfiguration
+	static class AccountServiceImplTestContextConfiguration {
+
+		@Bean
+		public AccountService accountService() {
+			return new AccountServiceImpl();
+		}
+	}
+
+	@Autowired
+	private AccountService accountService;
+
+	@MockBean
+	private AccountDao accountDao;
+
+	@Before
+	public void setUp() {
+		Account account = new Account();
+		account.setUsername("aneri.parikh");
+		account.setPassword("aneri");
+		Mockito.when(accountDao.validateUser(account.getUsername(), account.getPassword())).thenReturn(account);
+		System.out.println(account.getUsername());
+	}
+
+	@Test
+	public void whenValidName_thenEmployeeShouldBeFound() {
+		String username = "aneri.parikh";
+		String password = "aneri";
+		ResponseUtils validateUser = accountService.validateUser(username, password);
+		System.out.println(validateUser.getData());
+		System.out.println(validateUser.getMessage());
+		assertThat(validateUser.getData()).isEqualTo("");
+	}
 }
--- src/test/java/com/cognitivescale/service/BeneficiaryServiceImplTest.java
+++ src/test/java/com/cognitivescale/service/BeneficiaryServiceImplTest.java
@@ -1,20 +1,53 @@
 package com.cognitivescale.service;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cognitivescale.dao.BeneficiaryDao;
+import com.cognitivescale.entity.Beneficiary;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BeneficiaryServiceImplTest {
 
+	@TestConfiguration
+    static class EmployeeServiceImplTestContextConfiguration {
+  
+        @Bean
+        public BeneficiaryService beneficiaryService() {
+            return new BeneficiaryServiceImpl();
+        }
+    }
+	
 	@Autowired
 	private BeneficiaryService beneficiaryService;
 	
 	@MockBean
 	private BeneficiaryDao beneficiaryDao;
+	
+	@Before
+	public void setUp() {
+	    Beneficiary beneficiary = new Beneficiary();
+	    beneficiary.setAccountNumber(946786736);
+	    Mockito.when(beneficiaryDao.findByBeneficiaryAccountNumber(beneficiary.getAccountNumber())).thenReturn(beneficiary);
+	}
+	
+//	@Test
+//	public void whenValidName_thenEmployeeShouldBeFound() {
+//	   Integer beneficiaryAccountNumber = 946786736;
+//	   beneficiaryService.get
+//	    Employee found = employeeService.getEmployeeByName(name);
+//	    
+//	  
+//	     assertThat(found.getName())
+//	      .isEqualTo(name);
+//	 }
 }
