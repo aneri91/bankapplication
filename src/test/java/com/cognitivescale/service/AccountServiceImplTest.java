52be44b9ef70f138598a487407624ee925a2c4a4
ANERI\Aneri <aneri.parikh@infostretch.com>
2018-07-08 14:12:58 +0530

    Test cases for account service class

--- src/test/java/com/cognitivescale/service/AccountServiceImplTest.java
+++ src/test/java/com/cognitivescale/service/AccountServiceImplTest.java
@@ -2,6 +2,10 @@ package com.cognitivescale.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Random;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +19,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cognitivescale.dao.AccountDao;
 import com.cognitivescale.entity.Account;
+import com.cognitivescale.model.AccountModel;
+import com.cognitivescale.util.PasswordUtils;
 import com.cognitivescale.util.ResponseUtils;
 
 @RunWith(SpringRunner.class)
@@ -37,21 +43,52 @@ public class AccountServiceImplTest {
 	private AccountDao accountDao;
 
 	@Before
-	public void setUp() {
+	public void setUpAccountDetails() {
+
+		AccountModel accountModel = buildAccountModel();
+
 		Account account = new Account();
-		account.setUsername("aneri.parikh");
-		account.setPassword("aneri");
-		Mockito.when(accountDao.validateUser(account.getUsername(), account.getPassword())).thenReturn(account);
-		System.out.println(account.getUsername());
+		account.setUsername(accountModel.getUsername());
+		account.setPassword(PasswordUtils.encrypt(accountModel.getPassword().getBytes()));
+		account.setCountry(accountModel.getCountry() != null ? accountModel.getCountry() : "India");
+		account.setPhoneNumber(accountModel.getPhoneNumber());
+		account.setCreatedDate(new Date());
+		account.setAccountType(accountModel.getAccountType() != null ? accountModel.getAccountType() : "Saving");
+		account.setRole("CS_USER");
+		Random random = new Random();
+		account.setAccountNumber(random.nextInt(900000000) + 100000000);
+		account.setBalance(accountModel.getBalance());
+		account.setCurrency(accountModel.getCurrency() != null ? accountModel.getCurrency() : "INR");
+
+		Mockito.when(accountDao.save(account)).thenReturn(account);
+		Mockito.when(accountDao.validateUser(accountModel.getUsername(), accountModel.getPassword()))
+				.thenReturn(account);
 	}
 
 	@Test
-	public void whenValidName_thenEmployeeShouldBeFound() {
+	public void createAccountDetailsIfUsernameNotExist() {
+		AccountModel accountModel = buildAccountModel();
+		ResponseUtils responseUtils = accountService.save(accountModel);
+		assertThat(responseUtils.getMessage()).isEqualTo("Account is created successfully.");
+	}
+
+	@Test
+	public void loginWithUsernameAndPassword() {
 		String username = "aneri.parikh";
 		String password = "aneri";
-		ResponseUtils validateUser = accountService.validateUser(username, password);
-		System.out.println(validateUser.getData());
-		System.out.println(validateUser.getMessage());
-		assertThat(validateUser.getData()).isEqualTo("");
+		ResponseUtils responseUtils = accountService.validateUser(username, PasswordUtils.encrypt(password.getBytes()));
+		assertThat(responseUtils.getMessage()).isEqualTo("User is already registered.");
+	}
+
+	private AccountModel buildAccountModel() {
+		AccountModel accountModel = new AccountModel();
+		accountModel.setUsername("aneri.parikh");
+		accountModel.setPassword("aneri");
+		accountModel.setConfirmPassword("aneri");
+		accountModel.setCountry("India");
+		accountModel.setPhoneNumber(9725575554L);
+		accountModel.setAccountType("Current");
+		accountModel.setBalance(BigDecimal.TEN);
+		return accountModel;
 	}
 }
